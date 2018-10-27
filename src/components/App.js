33e00453805191a2b923f5b9ec3b72f22f1f3bf9
Martin Lindahl <martin_lindahl9@hotmail.com>
2018-10-27 13:52:50 +0200

    Playing around with onClick, onChange, onSubmit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,41 @@
 import React, {Component} from 'react'
 
 class App extends Component {
+
+
+    onClick() {
+        alert('Clicked')
+    }
+
+    onChange(event) {
+        console.log(event.target.value)
+    }
+
+    onSubmit(event) {
+        event.preventDefault()
+        alert('Submitted')
+    }
+
     render() {
+
+        const title = 'This is Martin\'s app'
+        const list = ['Item 1', 'Item 2', 'Item 3']
+
         return (
         <div>
-            <h1>My first React App!</h1>
-        </div>)
+            <h1>{title}</h1>
+            <h3> 
+                {
+                    list.map(item => <div key={item} onClick={this.onClick}> item </div>)
+                }
+            </h3>
+            
+            <form onSubmit={this.onSubmit}>
+                <input onChange={this.onChange}></input>
+            </form>
+            
+        </div>
+        )
         
     }
 }
