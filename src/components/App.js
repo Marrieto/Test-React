78b85941ceaeb90691516951ae3b007ca310b301
Martin Lindahl <martin_lindahl9@hotmail.com>
2018-10-28 02:53:15 +0200

    Added edit functionality

--- src/components/App.js
+++ src/components/App.js
@@ -30,6 +30,7 @@ class App extends Component {
 
         this.onDelete = this.onDelete.bind(this)
         this.onAdd = this.onAdd.bind(this)
+        this.onEditSubmit = this.onEditSubmit.bind(this)
     }
 
     componentWillMount() {
@@ -65,6 +66,22 @@ class App extends Component {
         this.setState( {products: filteredProducts} )
     }
 
+    onEditSubmit(name, price, originalName)
+    {
+        let products = this.getProducts()
+        
+        products = products.map(product => {
+            if (product.name === originalName) {
+                product.name = name
+                product.price = price
+            }
+
+            return product
+        })
+
+        this.setState({ products })
+    }
+
     render() {
         return (
 
@@ -82,6 +99,7 @@ class App extends Component {
                         key={product.name}
                         {...product}
                         onDelete={this.onDelete}
+                        onEditSubmit={this.onEditSubmit}
                         />
                             )
                 })
